9b66d4063c1f05e3d39236d32c9113d67a40ae9e
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-02 20:41:10 -0500

    test: useIsDesktop hook

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,10 +1,12 @@
-import { useScreenSize } from '../../src'
+import { useIsDesktop, useScreenSize } from '../../src'
 
 export function App() {
   const size = useScreenSize();
+  const isDesktop = useIsDesktop(500);
   return (
     <div className='container'>
       <h1>{size.width}</h1>
+      <h1>{`${isDesktop}`}</h1>
     </div>
   )
 }
