21a18802c664ab79255b5759484eb39f9d8fd3e5
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-03 15:33:30 -0500

    chore: add useIsClient hook

---
+++ src/useIsClient.ts
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+/**
+ * `useIsClient` returns true if the code is running in the browser. Otherwise, it returns false if the code is running on the server.
+ * 
+ * @returns A boolean value indicating if the code is running in the browser.
+ * 
+ * @example
+ * ```tsx
+ * const isClient = useIsClient();
+ * ```
+ * 
+ * @author Sebastian Marat Urdanegui Bisalaya <sebastianurdanegui.com>
+ * 
+ * @since 0.0.1
+ * @version 0.0.1
+ * 
+ */
+export function useIsClient(): boolean {
+    const [isClient, setIsClient] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return isClient;
+}
