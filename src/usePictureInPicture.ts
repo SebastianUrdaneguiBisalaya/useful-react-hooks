8ddeee9f9f5a1e7eed5d014ffbb20bbaddef5be5
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-13 20:43:13 -0500

    chore: add files for more hooks

---
+++ src/usePictureInPicture.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+/**
+ *
+ *
+ * @returns
+ *
+ * @example
+ * ```tsx
+ *
+ * ```
+ *
+ * @author Sebastian Marat Urdanegui Bisalaya <https://sebastianurdanegui.com>
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Picture-in-Picture_API
+ * @since 0.0.1
+ * @version 0.0.1
+ *
+ */
+export function usePictureInPicture() {}
---
+++ src/useSummarizer.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+/**
+ *
+ *
+ * @returns
+ *
+ * @example
+ * ```tsx
+ *
+ * ```
+ *
+ * @author Sebastian Marat Urdanegui Bisalaya <https://sebastianurdanegui.com>
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Summarizer_API#browser_compatibility
+ * @since 0.0.1
+ * @version 0.0.1
+ *
+ */
+export function useSummarizer() {}
