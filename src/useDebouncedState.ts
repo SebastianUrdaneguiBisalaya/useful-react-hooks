a41f3a1e2ecd081bf91fb35aafce4ec01051153c
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-14 19:28:10 -0500

    chore: add useDebouncedState hook

--- src/useDebouncedState.ts
+++ src/useDebouncedState.ts
@@ -1,13 +1,53 @@
 import * as React from 'react';
 
+export interface UseDebouncedStateOptions {
+  /**
+   * Delay in milliseconds before the debounced value is updated.
+   */
+  delay: number;
+}
+
+export interface UseDebouncedStateReturn<T> {
+  /**
+   * The inmediate (non-debounced) value.
+   */
+  value: T;
+
+  /**
+   * The debounced value, updated after the specified delay.
+   */
+  debouncedValue: T;
+
+  /**
+   * State setter for the inmediate value.
+   */
+  setValue: React.Dispatch<React.SetStateAction<T>>;
+}
+
 /**
+ * `useDebouncedState` is a React hook that manages a state value and exposes a debounced version of it.
+ * The debounced value is updated only after the specified delay has elapsed since the last change to the inmediate value.
  *
+ * @template T Type of the state value.
+ * @param optons Hook configuration options.
  *
- * @returns
+ * @returns An object containing the inmediate value, the debounced value, and a setter for the inmediate value.
  *
  * @example
  * ```tsx
- *
+ * const { value, debouncedValue, setValue } = useDebouncedState('', {
+ * delay: 400,
+ * });
+ * React.useEffect(() => {
+ *   if (!debouncedValue) return;
+ *   search(debouncedValue);
+ * }, [debouncedValue]);
+ * return (
+ *   <input
+ *     value={value}
+ *     onChange={(e) => setValue(e.target.value)}
+ *   />
+ * );
  * ```
  *
  * @author Sebastian Marat Urdanegui Bisalaya <https://sebastianurdanegui.com>
@@ -16,4 +56,33 @@ import * as React from 'react';
  * @version 0.0.1
  *
  */
-export function useDebouncedState() {}
+export function useDebouncedState<T>(
+  initialValue: T,
+  options: UseDebouncedStateOptions,
+): UseDebouncedStateReturn<T> {
+  const { delay } = options;
+  const [value, setValue] = React.useState<T>(initialValue);
+  const [debouncedValue, setDebouncedValue] = React.useState<T>(initialValue);
+
+  const timeoutRef = React.useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  React.useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    }
+  }, [value, delay]);
+
+  return {
+    value,
+    debouncedValue,
+    setValue,
+  }
+}
