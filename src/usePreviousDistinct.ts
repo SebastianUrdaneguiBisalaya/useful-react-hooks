b7d38e687d4489c8f873394f33f1933497a43572
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-19 20:41:21 -0500

    chore: add usePreviousDistinct hook

--- src/usePreviousDistinct.ts
+++ src/usePreviousDistinct.ts
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 /**
- *
+ * `usePreviousDistinct` is a React hook that returns the previous value of a state variable, but only if it is different from the current value.
  *
  * @returns
  *
  * @example
  * ```tsx
- *
+ * const prevUserId = usePreviousDistinct(userId);
  * ```
  *
  * @author Sebastian Marat Urdanegui Bisalaya <https://sebastianurdanegui.com>
@@ -16,4 +16,13 @@ import * as React from 'react';
  * @version 0.0.1
  *
  */
-export function usePreviousDistinct() {}
+export function usePreviousDistinct<T>(value: T): T | undefined {
+  const prevRef = React.useRef<T | undefined>(undefined);
+  const currentRef = React.useRef<T | undefined>(undefined);
+
+  if (!Object.is(value, currentRef.current)) {
+    prevRef.current = currentRef.current;
+    currentRef.current = value;
+  }
+  return prevRef.current;
+}
