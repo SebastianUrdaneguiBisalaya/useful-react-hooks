bba736904e75a12d4d12cb3f75a1193e6e3d9325
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-19 20:36:20 -0500

    chore: add useResettableState hook

--- src/useResettableState.ts
+++ src/useResettableState.ts
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 /**
+ * `useResettableState` is a React state hook with an explicit reset mechanism.
  *
- *
- * @returns
+ * @returns A state value, a setter, and a reset function.
  *
  * @example
  * ```tsx
- *
+ * const [count, setCount, reset] = useResettableState(0);
  * ```
  *
  * @author Sebastian Marat Urdanegui Bisalaya <https://sebastianurdanegui.com>
@@ -16,4 +16,19 @@ import * as React from 'react';
  * @version 0.0.1
  *
  */
-export function useResettableState() {}
+export function useResettableState<T>(
+  initialState: T | (() => T)
+): readonly [T, React.Dispatch<React.SetStateAction<T>>, () => void] {
+  const initialRef = React.useRef<T>(initialState instanceof Function ? initialState() : initialState);
+  const [state, setState] = React.useState<T>(initialState);
+
+  const reset = React.useCallback(() => {
+    setState(
+      typeof initialRef.current === 'function'
+        ? (initialRef.current as () => T)()
+        : initialRef.current
+    )
+  }, []);
+
+  return [state, setState, reset] as const;
+}
