41a0d97f15d3bfbec0144681c5adaffdcf4f1bf1
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-21 14:08:53 -0500

    chore: change image for text

--- website/src/components/shared/Footer.tsx
+++ website/src/components/shared/Footer.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image"
-
 export default function Footer() {
   return (
-    <footer className="py-6 border-t border-white/20 w-full flex flex-col md:flex-row items-start md:items-end gap-3 md:gap-2 p-8">
-      <Image src="/vibeHooks.png" alt="vibeHooks logo" width={100} height={100} />
-      <span className="font-sora text-[10px] text-white/50 md:leading-2">- developed by <a target="_blank" href="https://sebastianurdanegui.com">Sebastian Marat Urdanegui Bisalaya</a></span>
+    <footer className="py-6 border-t border-white/20 w-full flex flex-col md:flex-row items-start md:items-center md:gap-2 p-8">
+      <div>
+        <span className="font-jersey-15 text-2xl">vibe</span>
+        <span className="font-jersey-15 text-purple-500 text-2xl">Hooks</span>
+      </div>
+      <span className="font-sora text-[10px] text-white/50 md:leading-2">- developed by <a target="_blank" className="hover:text-white text-white/50 transition-all duration-500 ease-in-out" href="https://sebastianurdanegui.com">Sebastian Marat Urdanegui Bisalaya</a></span>
     </footer>
   )
 }
