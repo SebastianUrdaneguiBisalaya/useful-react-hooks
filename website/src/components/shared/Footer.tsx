3abae84de0833fa1292569c3ff535fb7aefcf21d
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-21 18:55:31 -0500

    chore: add links in footer and navbar

--- website/src/components/shared/Footer.tsx
+++ website/src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-6 border-t border-white/20 w-full flex flex-col md:flex-row items-start md:items-center md:gap-2 p-8">
+    <footer className="py-6 border-t border-white/20 w-full flex flex-col md:flex-row items-start md:items-center md:gap-2">
       <div>
         <span className="font-jersey-15 text-2xl">vibe</span>
         <span className="font-jersey-15 text-purple-500 text-2xl">Hooks</span>
