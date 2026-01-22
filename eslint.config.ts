8cf1e882f5c7cc746b76a4f3295b871672d8d827
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-22 15:19:36 -0500

    chore: install eslint package

---
+++ eslint.config.ts
@@ -0,0 +1,61 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import perfectionist from 'eslint-plugin-perfectionist'
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js, perfectionist },
+  rules: {
+    'perfectionist/sort-classes': [
+				'error',
+				{
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					groups: [
+						['builtin', 'external'],
+						['internal', 'parent', 'sibling', 'index'],
+					],
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+			'perfectionist/sort-jsx-props': [
+				'error',
+				{
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+			'perfectionist/sort-object-types': [
+				'error',
+				{
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+			'perfectionist/sort-objects': [
+				'error',
+				{
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+			'perfectionist/sort-variable-declarations': [
+				'error',
+				{
+					order: 'asc',
+					type: 'natural',
+				},
+			],
+  },
+  extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
