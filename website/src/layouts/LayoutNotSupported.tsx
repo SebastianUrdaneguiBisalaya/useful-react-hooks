9b8d89bd70daf32d2b8ef0fb3639e94fe4457c2b
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-25 11:42:18 -0500

    chore: add more layouts

---
+++ website/src/layouts/LayoutNotSupported.tsx
@@ -0,0 +1,9 @@
+export default function LayoutNotSupported({ title }: { title: string }) {
+  return (
+    <div className="w-full max-w-2xl border border-white/20 p-4 flex flex-col items-center justify-center bg-neutral-900 rounded-lg shadow-md">
+      <h3 className="text-white/60 text-center font-sora text-lg font-semibold">
+        {title}
+      </h3>
+    </div>
+  )
+}
