5da0d9a0dee57e0b1ba98e8dda97d496e20d551f
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-25 16:27:53 -0500

    styles: improve ui for mobile

--- website/src/app/docs/layout.tsx
+++ website/src/app/docs/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/shared/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-    <div className="p-8">
+    <div className="p-6 md:p-8">
       <Navbar />
       <article>
         {children}
