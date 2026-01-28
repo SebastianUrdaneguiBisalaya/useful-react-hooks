01bdd0f61d35f546c6304e1d07608e2790ee58d0
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-28 18:44:53 -0500

    chore: add demo

--- website/src/app/docs/productivity-and-utilities/useCopyToClipboard/Demo.tsx
+++ website/src/app/docs/productivity-and-utilities/useCopyToClipboard/Demo.tsx
@@ -1,11 +1,28 @@
+'use client';
+
+import { useCopyToClipboard } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo"
 
 export default function Demo() {
+  const { textCopied, copyToClipboard } = useCopyToClipboard();
+  const sampleText = "Hello world! ✋🏻";
   return (
     <LayoutDemo
-      title="useCopyToClipboard"
+      title="Copy to Clipboard"
     >
-      <div>TODO</div>
+      <button
+        onClick={() => copyToClipboard(sampleText)}
+        className="font-reddit-sans text-sm text-white/80 hover:text-white/90 transition-colors duration-500 ease-in-out border border-white/40 px-4 py-3 rounded-md cursor-pointer bg-neutral-950 hover:bg-neutral-950"
+      >
+        Copy to clipboard
+      </button>
+      {
+        textCopied && (
+          <span className="font-reddit-sans text-sm text-green-500/80">
+            Successfully copied: <span className="text-white font-medium">{textCopied}</span>
+          </span>
+        )
+      }
     </LayoutDemo>
   )
 }
