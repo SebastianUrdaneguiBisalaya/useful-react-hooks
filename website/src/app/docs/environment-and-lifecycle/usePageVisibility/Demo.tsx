99d1c2ff6a425f49a58cff8c837197600508bc20
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-29 19:03:20 -0500

    chore: add content for hook

--- website/src/app/docs/environment-and-lifecycle/usePageVisibility/Demo.tsx
+++ website/src/app/docs/environment-and-lifecycle/usePageVisibility/Demo.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import { usePageVisibility } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo";
 
 export default function Demo() {
+  const { isVisible, visibilityState } = usePageVisibility();
 	return (
 		<LayoutDemo
-      title="useIsClient"
+      title="Page Visibility"
     >
-			<div>This is a client-side component.</div>
+      <p className="text-sm text-center w-full text-white/60 font-reddit-sans">Document Status: <span className="font-bold text-white">{visibilityState}</span></p>
+      <p className="font-reddit-sans text-white/60 w-full text-center text-sm">Is Visible: <span className="font-bold text-white">{isVisible ? '✅' : '❌'}</span></p>
+      <p className="text-xs font-reddit-sans text-white/40">
+        (Try switching tabs and coming back to see the state change)
+      </p>
 		</LayoutDemo>
 	);
 }
