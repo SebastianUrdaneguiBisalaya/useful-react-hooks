c602ea298c0bd6c58b61b2f9719546394ea7e4a5
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-30 15:09:13 -0500

    chore: add content for hook

--- website/src/app/docs/multimedia/useAudio/Demo.tsx
+++ website/src/app/docs/multimedia/useAudio/Demo.tsx
@@ -1,11 +1,25 @@
 'use client';
 
+import { useAudio } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo";
+import { Button } from "@/components/ui/Button";
 
 export default function Demo() {
+  const audio = useAudio({ src: '/music.mp3' });
   return (
-    <LayoutDemo>
-
+    <LayoutDemo
+      title="Audio"
+    >
+      <h4 className="font-reddit-sans text-white/70 text-center">Audio status: <span className="font-bold text-white">{audio.status}</span></h4>
+      {audio.error && <p className="text-red-500 font-semibold">{audio.error.message}</p>}
+      <div className="flex flex-row items-center gap-2">
+        <Button.Primary onClick={audio.play}>
+          Play
+        </Button.Primary>
+        <Button.Secondary onClick={audio.pause}>
+          Pause
+        </Button.Secondary>
+      </div>
     </LayoutDemo>
   )
 }
