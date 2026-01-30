436e5affefb49509d645c72fcc27c196d149fbd4
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-30 15:50:41 -0500

    styles: improve ui

--- website/src/layouts/LayoutNotMounted.tsx
+++ website/src/layouts/LayoutNotMounted.tsx
@@ -1,10 +1,12 @@
 export default function LayoutNotMounted() {
   return (
-    <div className="flex w-full max-w-lg flex-row items-center justify-center rounded-lg border border-white/20 bg-neutral-900 p-4 shadow-md">
-      <div className="relative flex items-center">
-        <div className="h-5 w-5 rounded-full border-2 border-purple-500 border-t-transparent animate-spin" />
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="flex w-full max-w-lg flex-row items-center justify-center rounded-lg border border-white/20 bg-neutral-900 p-4 shadow-md">
+        <div className="relative flex items-center">
+          <div className="h-5 w-5 rounded-full border-2 border-purple-500 border-t-transparent animate-spin" />
+        </div>
+        <h3 className="pl-4 font-sora text-center font-medium text-white/60">This component is loading.</h3>
       </div>
-      <h3 className="pl-4 font-sora text-left font-medium text-white/60">This component is loading.</h3>
     </div>
   )
 }
