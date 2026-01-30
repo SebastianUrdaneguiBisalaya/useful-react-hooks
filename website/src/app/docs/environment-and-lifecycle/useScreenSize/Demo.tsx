28d36d3149e47ddbe7d2525d0b22e2db8512bee5
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-29 19:08:50 -0500

    chore: add content for hook

--- website/src/app/docs/environment-and-lifecycle/useScreenSize/Demo.tsx
+++ website/src/app/docs/environment-and-lifecycle/useScreenSize/Demo.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import { useScreenSize } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo";
 
 export default function Demo() {
-	return (
+	const { width, height } = useScreenSize();
+
+  return (
 		<LayoutDemo
-      title="useIsClient"
+      title="Screen Size"
     >
-			<div>This is a client-side component.</div>
+      <div className="flex flex-col items-center max-w-xs w-full bg-neutral-900 border border-white/50 border-dashed px-2 py-6">
+        <p className="font-reddit-sans font-bold text-white/90 text-center w-full">{width} x {height}</p>
+      </div>
+      <p className="text-xs font-reddit-sans text-white/40">
+        Resize your browser to see changes
+      </p>
 		</LayoutDemo>
 	);
 }
