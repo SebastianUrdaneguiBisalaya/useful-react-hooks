2dd58e6a9d6c225c2b73056474a5bbfe6a706bb4
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-30 14:24:49 -0500

    chore: add content for hook

--- website/src/app/docs/ui-and-layout/useIntersectionObserver/Demo.tsx
+++ website/src/app/docs/ui-and-layout/useIntersectionObserver/Demo.tsx
@@ -1,7 +1,30 @@
+'use client';
+
+import { useIntersectionObserver } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo";
+import { cn } from "@/lib/cn";
 
 export default function Demo() {
-  <LayoutDemo>
+  const { ref, isVisible } = useIntersectionObserver<HTMLDivElement>({
+    threshold: 0.5,
+    once: true,
+  });
 
-  </LayoutDemo>
+  return (
+    <LayoutDemo
+      title="Intersection Observer"
+    >
+      <div
+        ref={ref}
+        className={cn(
+          'transition-all duration-700 rounded-2xl bg-neutral-900 border border-white/20 p-4',
+          isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+        )}
+      >
+        <p className="font-reddit-sans text-sm text-white w-full text-center">
+          Visible on scroll 👀
+        </p>
+      </div>
+    </LayoutDemo>
+  )
 }
