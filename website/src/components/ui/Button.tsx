5973c513760f61e099e52d1ff6681fc5a47a8456
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-30 14:51:53 -0500

    fix: types

--- website/src/components/ui/Button.tsx
+++ website/src/components/ui/Button.tsx
@@ -1,51 +1,63 @@
 import { cn } from "@/lib/cn";
+import React from "react";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children: React.ReactNode;
 }
 
-function BaseButton({
-  className,
-  children,
-  ...props
-}: ButtonProps) {
-  return (
-    <button
-      className={cn(
-        'w-fit font-reddit-sans text-sm transition-colors duration-500 ease-in-out px-4 py-1.5 rounded-lg cursor-pointer',
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  )
-}
+const BaseButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          'w-fit font-reddit-sans text-sm transition-colors duration-500 ease-in-out px-4 py-1.5 rounded-lg cursor-pointer focus:outline-none',
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+);
 
-function Primary(props: ButtonProps) {
-  return (
-    <BaseButton
-      {...props}
-      className={cn(
-        'bg-white-100 border border-transparent text-neutral-900',
-        props.className
-      )}
-    />
-  )
-}
+BaseButton.displayName = 'Button';
 
-function Secondary(props: ButtonProps) {
-  return (
-    <BaseButton
-      {...props}
-      className={cn(
-        'bg-neutral-900 hover:bg-neutral-800 border border-white/20 text-white/70 hover:text-white/90',
-        props.className
-      )}
-    />
-  )
-}
+const Primary = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    return (
+      <BaseButton
+        ref={ref}
+        {...props}
+        className={cn(
+          'bg-white-100 border border-transparent text-neutral-900',
+          props.className
+        )}
+      />
+    )
+  }
+);
+
+Primary.displayName = 'Button.Primary';
+
+const Secondary = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    return (
+      <BaseButton
+        ref={ref}
+        {...props}
+        className={cn(
+          'bg-neutral-900 hover:bg-neutral-800 border border-white/20 text-white/70 hover:text-white/90',
+          props.className
+        )}
+      />
+    )
+  }
+);
+
+Secondary.displayName = 'Button.Secondary';
 
 export const Button = Object.assign(BaseButton, {
   Primary,
