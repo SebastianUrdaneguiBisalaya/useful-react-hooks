fc537d0a13d5ac924c39c02b633de29a7140ba67
SebastianUrdaneguiBisalaya <sebasurdanegui@gmail.com>
2026-01-30 13:14:13 -0500

    chore: add content for hook

--- website/src/app/docs/ui-and-layout/useHoverIntent/Demo.tsx
+++ website/src/app/docs/ui-and-layout/useHoverIntent/Demo.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import { useHoverIntent } from "../../../../../../src";
 import LayoutDemo from "@/layouts/LayoutDemo";
+import { Button } from "@/components/ui/Button";
 
 export default function Demo() {
-  <LayoutDemo>
-
-  </LayoutDemo>
+  const hover = useHoverIntent({
+    delay: 150,
+    tolerance: 8,
+  });
+  return (
+    <LayoutDemo
+      title="Hover Intent"
+    >
+      <div className="flex flex-col items-center relative w-full">
+        <Button.Primary
+          {...hover.handlers}
+        >
+          Hover me
+        </Button.Primary>
+        {
+          hover.isIntent && (
+            <div className="bg-neutral-900 absolute top-full mt-2 rounded-xl border border-white/20 px-3 py-2">
+              <span className="font-reddit-sans font-semibold text-sm text-white">
+                Tooltip content 👌
+              </span>
+            </div>
+          )
+        }
+      </div>
+    </LayoutDemo>
+  )
 }
